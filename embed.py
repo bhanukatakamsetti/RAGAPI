78e078a3aa7306d222f9d75d28307d0a219b9d47
bhanu katakamsetti <bhanukatakamsetti246.16@gmail.com>
2026-01-11 13:01:07 +0530

    commiting the entire api

---
+++ embed.py
@@ -0,0 +1,20 @@
+
+
+import chromadb
+
+client = chromadb.PersistentClient(path="./chromadb")
+
+collection = client.get_or_create_collection("docs")
+
+
+with open("k8s.txt", "r") as file:
+  text = file.read()
+
+
+collection.add(documents=[text], ids=["k8s"])
+
+print("Embedding stored in Chroma")
+
+
+
+
